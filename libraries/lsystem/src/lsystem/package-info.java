9a27749862519c11ce39dcead41c65f3d2d207e0
Jérôme Benoit <jerome.benoit@piment-noir.org>
2016-09-28 17:30:03 +0200

    Add L-Systems processing library.
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ libraries/lsystem/src/lsystem/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * This library is compiled to work with processing 3.2.X (apart from 3D implantation,
+ * works OK with earlier versions of processing it provides a simple interface for 
+ * exploring LSystems using processing, see included examples for usage. However I 
+ * hope this crude documentation is also useful. 
+ * For more on LSystems see: http://algorithmicbotany.org/papers/
+ */
+package lsystem;
